1f8bae3a9c2fae7b31dbff4cb0a5829bf7784b6a
Richard Reynolds <richard.reynolds@redbull.com>
2021-11-26 05:37:35 +0100

    Add check that backend url set; return error if forgotten

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,16 @@ export const mockable = (handlerToDecorate: APIGatewayProxyHandler) => {
     if (event.headers['x-mock-via-accept'] === undefined) {
       return handlerToDecorate(event, context, callback);
     } else {
+      if (!process.env.ACCEPTJS_BACKEND) {
+        return {
+          statusCode: 500,
+          body: 'ACCEPTJS_BACKEND environment variable is unset',
+          headers: {
+            'Access-Control-Allow-Origin': event.headers.origin ?? ''
+          }
+        };
+      }
+
       const body = await new Promise<string>((resolve, reject) => {
         const options = {
           hostname: process.env.ACCEPTJS_BACKEND,
@@ -32,6 +42,7 @@ export const mockable = (handlerToDecorate: APIGatewayProxyHandler) => {
           .on('error', (reason) => reject(reason))
           .end();
       });
+
       return {
         statusCode: 200,
         body,
